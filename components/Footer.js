c12b3d92949827c62b6bff83391842ee3aab8efe
thordurf <thordur921@gmail.com>
2023-05-22 11:42:16 +0200

    smol changes

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer className={styles.footer}>
         <ul>
           <li>E-Mail: ayali@ayali.dk</li>
-          <li>CPR: 123456-1234</li>
+          <li>CVR: 123456-1234</li>
           <li>Tlf: 55 55 55 55</li>
           <li>Adrs: Porcælensvej 12567</li>
         </ul>
