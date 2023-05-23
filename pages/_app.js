d44fd5ff2f8d3c6a714013a92a6e6c3415b9a3c0
thordurf <thordur921@gmail.com>
2023-05-23 15:04:31 +0200

    form basic styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import "@styles/globals.scss";
-import "@styles/blog.scss"
-import "@styles/services.scss"
+import "@styles/blog.scss";
+import "@styles/services.scss";
 import "@styles/header.scss";
 import "@styles/footer.scss";
 import "@styles/index.scss";
+import "@styles/contact.scss";
 
 import Layout from "../components/Layout";
 
--- pages/contact.js
+++ pages/contact.js
@@ -17,51 +17,57 @@ function contact() {
           name="description"
           content="AYA LI - Digitial Marketing Consultant - Contact"
         ></meta>
+        <meta name="theme-color" content="#c7f3e9" />
       </Head>
-      <div>
-        <h1>Contact</h1>
-        <p>
-          Pricing of packages blabla lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum 30 MINUTE VIDEO CALLlorem ipsum Pricing of packages blabla lorem
-          ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum Pricing of
-          packages blabla lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-          ipsum
-        </p>
+      <div className="contact_wrapper">
+        <div className="form_wrapper">
+          <div className="contact_about">
+            <h1>Contact</h1>
+            <p>
+              Pricing of packages blabla lorem ipsum lorem ipsum lorem ipsum
+              lorem ipsum 30 MINUTE VIDEO CALLlorem ipsum Pricing of packages
+              blabla lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+              Pricing of packages blabla lorem ipsum lorem ipsum lorem ipsum
+              lorem ipsum lorem ipsum
+            </p>
+          </div>
+          <form name="contact" netlify>
+            <div className="input_wrapper">
+              <div className="input_field">
+                <input name="firstName" type="text" required />
+                <label htmlFor="firstName">First Name</label>
+              </div>
+              <div className="input_field">
+                <input name="lastName" type="text" required />
+                <label htmlFor="lastName">Last Name</label>
+              </div>
+              <div className="input_field">
+                <input name="company" type="text" required />
+                <label htmlFor="company">Company Name</label>
+              </div>
+              <div className="input_field">
+                <input name="email" type="email" required />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="text_field">
+                <textarea name="message" required></textarea>
+                <label htmlFor="email">Description</label>
+              </div>
+            </div>
+            <div className="acknowledge">
+              <input type="checkbox" required />
+              <span>
+                By sending this form, I confirm that I have read and
+                acknowledged the privacy policy.
+              </span>
+            </div>
+            <input className="cta submit" type="submit" value="SUBMIT" />
+          </form>
+        </div>
+        <div className="contact_image">
+          <Image src={portrait} alt="portrait of AYA LI" />
+        </div>
       </div>
-      <form>
-        <div className="input_wrapper">
-          <div className="input_field">
-            <input name="firstName" type="text" required />
-            <label htmlFor="firstName">First Name</label>
-          </div>
-          <div className="input_field">
-            <input name="lastName" type="text" required />
-            <label htmlFor="lastName">Last Name</label>
-          </div>
-          <div className="input_field">
-            <input name="company" type="text" required />
-            <label htmlFor="company">Company Name</label>
-          </div>
-          <div className="input_field">
-            <input name="email" type="text" required />
-            <label htmlFor="email">Email</label>
-          </div>
-          <div className="input_field">
-            <textarea name="message"></textarea>
-            <label htmlFor="email">Description</label>
-          </div>
-        </div>
-        <div className="acknowledge">
-          <input type="checkbox" required />
-          <span>
-            By sending this form, I confirm that I have read and acknowledged
-            the privacy policy.
-          </span>
-        </div>
-        <input type="submit" value="SUBMIT" />
-      </form>
-      <Image src={portrait} alt="image of aya" />
-
       <FooterNav currentPage={currentPage}></FooterNav>
     </>
   );
