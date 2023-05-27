84c10dab9872117afea9d21dc99812e5de2e213a
Mehmet <mehmet.h.yurdakul@gmail.com>
2023-05-27 14:54:23 +0200

    added more structure to company page

--- pages/company.js
+++ pages/company.js
@@ -18,12 +18,15 @@ function company() {
         ></meta>
         <meta name="theme-color" content="#c7f3e9" />
       </Head>
-
+      <div className="company-page-wrapper">
       <div className="company-hero">
         <h1>ABOUT ME</h1>
         <p>The internet is becoming the town square for the global village of tomorrow</p>
           <p>Bill Gates</p>
         <p>About me bla bla Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus.</p>
+        <div className="company-image">
+        <Image src={portrait} alt="portrait of AYA LI" />
+      </div>
       </div>
       <section className="block-content t-block-teal l-block-spacing">
 					<div className="l-contained">
@@ -61,12 +64,9 @@ function company() {
                 </div>
 							</li>
 						</ul>
-						
-								
-						
 					</div>
-
 				</section>
+        </div>
       <FooterNav currentPage={currentPage}></FooterNav>
     </>
   );
