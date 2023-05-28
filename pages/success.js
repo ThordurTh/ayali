4dd3775f643d72e7da65a30c12757cbda276a146
Mehmet <mehmet.h.yurdakul@gmail.com>
2023-05-28 17:19:16 +0200

    testing thank you message

---
+++ pages/success.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Head from "next/head";
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
+const router = useRouter();
+const { name } = router.query;
+
+function success() {
+    return (
+        <>
+          <Head>
+            <title>AYA LI - Success</title>
+            <meta name="keywords" content="Some, good, keywords"></meta>
+            <meta
+              name="description"
+              content="AYA LI - Digitial Marketing Consultant - Success"
+            ></meta>
+            <meta name="theme-color" content="#c7f3e9" />
+          </Head>
+    <section className="services-hero success-message">
+            <h1>Thank you {name} </h1>
+        <p>
+          Scale your eCommerce business by successfully overcoming your
+          marketing challenges with a tailored digital marketing strategy.
+        </p>
+        <div>
+            <Link href="../">
+              <a className="cta success">Home</a>
+            </Link>
+            </div>
+      </section>
+          </>
+  );
+}
+
+export default success
